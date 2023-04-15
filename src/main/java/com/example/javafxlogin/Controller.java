f5e0cf060a7ae12421df6530142f3f0ea6d42a1f
charanatchutanna <charanatchutanna@gmail.com>
2023-04-15 16:16:04 -0400

    1st version and 2nd Commit by charan

--- src/main/java/com/example/javafxlogin/Controller.java
+++ src/main/java/com/example/javafxlogin/Controller.java
@@ -2,17 +2,52 @@ package com.example.javafxlogin;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+
+import java.sql.SQLException;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    TextField username;
+    @FXML
+    TextField password1;
+    @FXML
+    Button loginButton;
+
+    public void loginAction() throws SQLException {
+
+        Window owner = loginButton.getScene().getWindow();
+
+        System.out.println(username.getText());
+        System.out.println(password1.getText());
+        if (username.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!", "Please enter your name");
+            return;
+        }
+
+        if (password1.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!", "Please enter your email id");
+            return;
+        }
+        String username1 = username.getText();
+        String password2 = password1.getText();
+
+
+        Entity entity = new Entity();
+        entity.insertRecord(username1, password2);
+
+        showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!", "Welcome " + username.getText());
     }
-
-    public void loginAction(ActionEvent event) {
+    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.javafxlogin {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.javafxlogin to javafx.fxml;
     exports com.example.javafxlogin;
