c1d465c82b210402e4a8684a2dde655a12a5688e
charanatchutanna <charanatchutanna@gmail.com>
2023-04-14 21:55:34 -0400

    Login Page 1st commit sample by charan on mac

---
+++ src/main/java/com/example/javafxlogin/Controller.java
@@ -0,0 +1,18 @@
+package com.example.javafxlogin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void loginAction(ActionEvent event) {
+    }
+}
